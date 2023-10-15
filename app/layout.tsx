6d171c3d9ec89ab4e74c857218e9c285d78b94d4
Rishikesh <rishikesh.kanabar@gmail.com>
2023-10-15 09:37:35 -0400

    feat: add theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 
+import { ThemeProvider } from "@/components/theme-provider"
+
 export const metadata: Metadata = {
   title: 'SaaS Ninja',
   description: 'SaaS Template For Lazy Developers',
@@ -12,8 +14,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Container from '@/components/Container'
+import Container from '@/components/container'
+import { ModeToggle } from '@/components/mode-toggle'
 
 export default function Home() {
   return (
     <Container>
-      <div className='flex items-center justify-center'>
-        SaaS Ninja
+      <div className='flex items-center justify-between py-4'>
+        <div>
+          <h1 className='text-3xl font-bold'>SaaS Ninja</h1>
+        </div>
+        <ModeToggle />
       </div>
     </Container>
   )
