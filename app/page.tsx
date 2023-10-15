d7025df6f819ea91a6a80d7e2e81b96273e6aa97
Rishikesh <rishikesh.kanabar@gmail.com>
2023-10-15 14:32:43 -0400

    feat: add simple navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
+"use client"
+
 import Container from '@/components/container'
-import { ModeToggle } from '@/components/mode-toggle'
+import Navbar from '@/components/navbar'
 
 export default function Home() {
   return (
     <Container>
-      <div className='flex items-center justify-between py-4'>
-        <div>
-          <h1 className='text-3xl font-bold'>SaaS Ninja</h1>
-        </div>
-        <ModeToggle />
-      </div>
+      <Navbar />
     </Container>
   )
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link';
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
+import ModeToggle from './mode-toggle';
+import Container from './container';
+
+export default function Navbar() {
+  return (
+    <Container>
+      <div className='flex items-center justify-between py-4'>
+        <div className='flex flex-row'>
+          <Link href="/" legacyBehavior passHref>
+            <a className='text-3xl font-bold'>SaaS Ninja</a>
+          </Link>
+          <NavigationMenu className='list-none pl-2'>
+            <NavigationMenuItem>
+            <Link href="/features" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Features
+                </NavigationMenuLink>
+              </Link>
+              <Link href="/pricing" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Pricing
+                </NavigationMenuLink>
+              </Link>
+              <Link href="/blog" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Blog
+                </NavigationMenuLink>
+              </Link>
+              <Link href="/docs" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Documentation
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenu>
+        </div>
+        <ModeToggle />
+      </div>
+    </Container>
+  );
+}
