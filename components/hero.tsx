b8fef4ee4a9a1e5d2d8569119c2c8979f8f31acd
Rishikesh <rishikesh.kanabar@gmail.com>
2023-10-17 11:25:47 -0400

    feat: add work in progress tag

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,6 +8,12 @@ const Hero = () => {
     <Container>
       <div className="grid px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
+          <Button
+            variant="outline"
+            className="px-5 py-3 rounded-full mb-4 hover:bg-inherit pointer-events-none"
+          >
+            Work In Progress
+          </Button>
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
             The Ultimate SaaS Development Template
           </h1>
